44e70540860833b83faac019ec4c9930c1937ecc
Aashihegu <aashihegu1@gmail.com>
2026-03-07 13:36:33 -0800

    commit

--- assets/js/GameEnginev1/GameLevelPirateHunt.js
+++ assets/js/GameEnginev1/GameLevelPirateHunt.js
@@ -42,7 +42,7 @@ class GameLevelPirateHunt {
         const npcData1 = {
             id: 'Captain Hook',
             greeting: 'Shiver me timbers! How did ye make it onto me ship??',
-            src: path + "/images/hink;gamebuilder/sprites/Hook.png",
+            src: path + "/images/gamebuilder/sprites/Hook.png",
             SCALE_FACTOR: 4,
             ANIMATION_RATE: 1000000008,
             INIT_POSITION: { x: 857, y: 400 },
