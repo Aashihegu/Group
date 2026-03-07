23f0ec4fcba2bc902c4ba9f70dad7fe0f1c6f607
Aashihegu <aashihegu1@gmail.com>
2026-03-06 22:16:39 -0800

    added

--- assets/js/GameEnginev1/GameLevelPirateHunt.js
+++ assets/js/GameEnginev1/GameLevelPirateHunt.js
@@ -25,6 +25,7 @@ class GameLevelPirateHunt {
             INIT_POSITION: { x: 150, y: 470 },
             pixels: { height: 256, width: 256 },
             orientation: { rows: 4, columns: 4 },
+
             down: { row: 0, start: 0, columns: 4 },
             downRight: { row: Math.min(2, 4 - 1), start: 0, columns: 3, rotate: Math.PI / 16 },
             downLeft: { row: Math.min(1, 4 - 1), start: 0, columns: 3, rotate: -Math.PI / 16 },
@@ -33,6 +34,7 @@ class GameLevelPirateHunt {
             up: { row: Math.min(3, 4 - 1), start: 0, columns: 4 },
             upRight: { row: Math.min(2, 4 - 1), start: 0, columns: 3, rotate: -Math.PI / 16 },
             upLeft: { row: Math.min(1, 4 - 1), start: 0, columns: 3, rotate: Math.PI / 16 },
+
             hitbox: { widthPercentage: 0.45, heightPercentage: 0.2 },
             keypress: { up: 87, left: 65, down: 83, right: 68 }
         };
@@ -44,13 +46,17 @@ class GameLevelPirateHunt {
             SCALE_FACTOR: 5,
             ANIMATION_RATE: 1000000008,
             INIT_POSITION: { x: 857, y: 470 },
+
             pixels: { height: 120, width: 335 },
             orientation: { rows: 1, columns: 3 },
             down: { row: 0, start: 0, columns: 3 },
+
             hitbox: { widthPercentage: 0.4, heightPercentage: 0.6 },
+
             dialogues: [
                 'Fear not! We will get ye out of here... once we find a way through that pesky barrier!'
             ],
+
             reaction: function () {
                 if (this.dialogueSystem) {
                     this.showReactionDialogue();
@@ -58,6 +64,7 @@ class GameLevelPirateHunt {
                     console.log(this.greeting);
                 }
             },
+
             interact: function () {
                 if (this.dialogueSystem) {
                     this.showRandomDialogue();
@@ -79,7 +86,7 @@ class GameLevelPirateHunt {
         const barrier_floor = {
             id: 'barrier_floor',
             x: 0,
-            y: 570,
+            y: 620,
             width: 1280,
             height: 10,
             visible: true,
@@ -90,7 +97,7 @@ class GameLevelPirateHunt {
         const barrier_hole = {
             id: 'barrier_hole',
             x: 520,
-            y: 570,
+            y: 620,
             width: 250,
             height: 50,
             visible: true,
